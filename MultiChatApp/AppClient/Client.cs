771437c6d58960dfbf00106f3e6a6bb2703f0699
M. Yahya Avar <89209381+myahyavar@users.noreply.github.com>
2022-05-24 01:14:37 +0300

    minor client additions

--- MultiChatApp/AppClient/Client.cs
+++ MultiChatApp/AppClient/Client.cs
@@ -19,8 +19,28 @@ namespace AppClient
 
         public void Connect(String server)
         {
+        try
+         {
             int port = 8888;
             client = new TcpClient(server,port);
+
+            NetworkStream stream = client.GetStream();
+
+            int count = 0;
+            while (count++ < 3)
+            {
+
+                Byte[] data = System.Text.Encoding.ASCII.GetBytes("connect");
+                stream.Write(data, 0, data.Length);
+
+                    // need to build a server - like handler for data streams
+            }
+
+        } 
+        catch (Exception e) 
+        {
+                client = null;
         }
+}
     }
 }
